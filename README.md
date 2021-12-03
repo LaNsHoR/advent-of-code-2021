623917d8e81aedc4bd30f8ae883a48df88257148
Ernesto Azuar <ernesto.azuar@performancehorizon.com>
2021-12-03 21:18:55 +0100

    Day 3

--- README.md
+++ README.md
@@ -1,13 +1,21 @@
 # advent-of-code-2021
 
+This solutions REQUIRE the associated data file in the same folder.
+
 # Day 1: Javascript
 To run the solution:
 ```bash
 node d1.js
 ```
 
-# Day 2: C + x64 Assembly
+# Day 2: C and x64 Assembly
 To run the solution:
 ```bash
 gcc d2.c && ./a.out
+```
+
+# Day 3: Python
+To run the solution:
+```bash
+python d3.py
 ```
---
+++ d3.py
@@ -0,0 +1,61 @@
+file = open('d3_data.txt', "r")
+raw_data = file.readlines()
+file.close()
+
+# Common Utils ===============================================
+
+def parse_data( data ):
+    return { "base":data.strip(), "volatile": data.strip() }
+
+def common_bit( data, key, i = 0 ):
+    one_count = 0
+    for word in data:
+        if word[key][i] == '1':
+            one_count += 1
+    if one_count > len(data)/2.0:
+        return 1
+    if one_count < len(data)/2.0:
+        return 0
+    return 2
+
+# Part 1 =====================================================
+
+def solver_1( data ):
+    gamma = ''
+    size = len(data[0]["base"])
+    for i in range( size ):
+        gamma += '1' if common_bit(data, "base", i) else '0'
+    gamma = int(gamma, 2)
+    epsilon = ~gamma
+    epsilon &= 0xFFFFFFFF >> ( 32 - size )
+    print(gamma, epsilon, gamma * epsilon)
+
+# Part 2 =====================================================
+
+def process_queue( queue, criteria):
+    while( len(queue) > 1 ):
+        new_queue = []
+        bit = common_bit( queue, "volatile" )
+        for word in queue:
+            first = word["volatile"][0]
+            # Last word found
+            if( len( queue ) + len(new_queue) == 1 ):
+                return word["base"]
+            # Compare criteria
+            if( (bit == 0 and first == criteria[0]) or
+                (bit == 1 and first == criteria[1]) or
+                (bit == 2 and first == criteria[2]) ):              
+                    new_queue.append( { "base":word["base"], "volatile":word["volatile"][1:]} )
+        queue = new_queue
+    return queue[0]["base"]
+
+def solver_2( queue ):
+    o2 = int( process_queue(queue, ['0', '1', '1']), 2)
+    co2 = int( process_queue(queue, ['1', '0', '0']), 2)
+    print( o2, co2, o2*co2 )
+
+# Go go go! ==================================================
+
+queue = map( parse_data, raw_data )
+solver_1( queue )
+solver_2( queue )
